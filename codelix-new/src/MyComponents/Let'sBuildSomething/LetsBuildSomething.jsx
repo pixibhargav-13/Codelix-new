153f05c17dfc2e55d2b47e3447d0584acdbda759
Yatri-patel <yatripatel02@gmail.com>
2025-11-29 00:09:12 +0530

    Lets Build Something desktop screen

--- codelix-new/src/MyComponents/Let'sBuildSomething/LetsBuildSomething.jsx
+++ codelix-new/src/MyComponents/Let'sBuildSomething/LetsBuildSomething.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import "./LetsBuildSomething.css";
 import rightArrow from "../Images/whiteArrow.png";
-import buildSomething from "../Images/Build-something.png"; // Curved left/right SVG or PNG
+import buildSomething from "../Images/Build-something.png";
+import buildSomethingMobile from "../Images/lets-build-mobile-screen.png"; // Add your round circle image
 
 export const LetsBuildSomething = () => {
   return (
     <section className="lets-build-section">
-      <div className="overlay-curves">
+      {/* Desktop curves */}
+      <div className="overlay-curves desktop-curves">
         <img
           src={buildSomething}
-          alt="curve-left"
-          className="curve curve-left"
+          alt="decorative curves"
+          className="curve-background"
         />
+      </div>
+
+      {/* Mobile round circle */}
+      <div className="overlay-curves mobile-curves">
         <img
-          src={buildSomething}
-          alt="curve-left"
-          className="curve curve-right"
+          src={buildSomethingMobile}
+          alt="decorative circle"
+          className="curve-background-mobile"
         />
       </div>
 
       <div className="content-wrapper text-center">
         <h2 className="lets-build-main-title">
-          Let’s build something that <br /> works beautifully.
+          Let's build something that <br /> works beautifully.
         </h2>
 
         <p className="lets-build-subtitle">
-          Tell us about your idea, and we’ll help you make it real, faster and
+          Tell us about your idea, and we'll help you make it real, faster and
           better.
         </p>
 
